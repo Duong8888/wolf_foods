1312c749d2d31711cb87065023cfbd2b30ea87f4
HaiDuong003 <102920748+HaiDuong003@users.noreply.github.com>
2022-10-04 14:56:37 +0700

    update_database.php

--- model/databse.php
+++ model/databse.php
@@ -1,4 +1,5 @@
 <?php
+require './connect.php';
 // global $conn;
 // function connect()
 // {
@@ -12,5 +13,48 @@
 //   return $conn;
 // }
 
+// lấy tất cả giá trị của bảng
+function allValues($table)
+{
+  // $conn = connect();
+  $sql = "
+        SELECT * FROM $table";
+  connect($sql);
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute();
+}
+
+// thêm user vào bảng user 
+function addUser($username, $password, $email, $avatar)
+{
+  $sql = "
+  INSERT INTO `user`(`username`, `password`, `email`, `avatar`) 
+  VALUES (?,?,?,?);
+  ";
+  connect($username, $password, $email, $avatar);
+  // $tmt = $conn->prepare($sql);
+  // // $conn = connect();
+  // $tmt->execute([$username, $password, $email, $avatar]);
+}
+
+// // xóa trường dữ liệu trong bảng 
+// function delete($table, $column, $id)
+// {
+//     $conn = connect();
+//     $sql = "
+//         DELETE FROM $table WHERE $column = $id";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute();
+// }
+
+// // update user 
+function updateUser($username, $password, $email, $avatar, $idU)
+{
+  // $conn = connect();
+  $sql = "
+            UPDATE `user` SET `username` = '$username', `password` = '$password', `email` = '$email', `avatar` = '$avatar' 
+            WHERE `user`.`id` = $idU;";
+  connect($sql);
+}
 
 // connect();
