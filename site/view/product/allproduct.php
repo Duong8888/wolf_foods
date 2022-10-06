a3e637c10f60a4d380b21f4e1312117df3b19455
Duong8888 <88546189+Duong8888@users.noreply.github.com>
2022-10-06 10:09:22 +0700

    upload

--- site/view/product/allproduct.php
+++ site/view/product/allproduct.php
@@ -5,31 +5,51 @@ $allProduct = pagination($table, $row);
 $countpage = countPages($row);
 $queryCategories = "SELECT * FROM categories";
 $getCategories = getAll($queryCategories);
+$arrFilterPrice = [
+    [
+        'name' => 0,
+        'text' => "Giá dưới 100.000đ"],
+    [
+        'name' => 1,
+        'text' => "100.000đ - 200.000đ"],
+    [
+        'name' => 2,
+        'text' => "200.000đ - 300.000đ"],
+    [
+        'name' => 3,
+        'text' => "300.000đ - 500.000đ"],
+    [
+        'name' => 4,
+        'text' => "500.000đ - 1.000.000đ"],
+    [
+        'name' => 5,
+        'text' => "Giá trên 1000.000đ"],
+];
 ?>
 <main>
     <div class="box-container">
         <div class="box-filter">
             <div class="filter">
                 <p class="filter__title">Bộ lọc sản phẩm</p>
-                <div class="filter__box-item">
-                    <p class="filter__naem-item">Danh mục</p>
-                    <ul>
-                        <?php foreach ($getCategories as $value): ?>
-                        <li><input type="checkbox" name="" id="<?=$value['id_categories']?>"> <label for="<?=$value['id_categories']?>"><?=$value['category_name']?></label></li>
-                        <?php endforeach?>
-                    </ul>
-                </div>
-                <div class="filter__box-item">
-                    <p class="filter__naem-item">Giá tiền</p>
-                    <ul>
-                        <li><input type="checkbox" name="" id="checkbox6"> <label for="checkbox6">Giá dưới 100.000đ</label></li>
-                        <li><input type="checkbox" name="" id="checkbox7"> <label for="checkbox7">100.000đ - 200.000đ</label></li>
-                        <li><input type="checkbox" name="" id="checkbox8"> <label for="checkbox8">200.000đ - 300.000đ</label></li>
-                        <li><input type="checkbox" name="" id="checkbox9"> <label for="checkbox9">300.000đ - 500.000đ</label></li>
-                        <li><input type="checkbox" name="" id="checkbox10"> <label for="checkbox10">500.000đ - 1.000.000đ</label></li>
-                        <li><input type="checkbox" name="" id="checkbox11"> <label for="checkbox11">Giá trên 1.000.000đ</label></li>
-                    </ul>
-                </div>
+                <form action="index.php?action=all-product" method="POST">
+                    <button type="submit" class="search-btn">Tìm kiếm</button>
+                    <div class="filter__box-item">
+                        <p class="filter__naem-item">Danh mục</p>
+                        <ul>
+                            <?php foreach ($getCategories as $value): ?>
+                                <li><input type="checkbox" name="<?=$value['id_categories']?>" id="<?=$value['id_categories']?>"> <label for="<?=$value['id_categories']?>"><?=$value['category_name']?></label></li>
+                            <?php endforeach?>
+                        </ul>
+                    </div>
+                    <div class="filter__box-item">
+                        <p class="filter__naem-item">Giá tiền</p>
+                        <ul>
+                        <?php foreach ($arrFilterPrice as $key => $value): ?>
+                                <li><input type="checkbox" name="<?=$value['name']?>" id="<?=$key?>"> <label for="<?=$key?>"><?=$value['text']?></label></li>
+                            <?php endforeach?>
+                        </ul>
+                    </div>
+                </form>
             </div>
         </div>
         <div class="box-product">
@@ -48,7 +68,7 @@ $getCategories = getAll($queryCategories);
                 <nav aria-label="...">
                     <ul class="pagination pagination-sm">
                         <?php for ($item = 1; $item <= $countpage; $item++): ?>
-                        <li class="page-item <?=$_GET['page'] == $item ? "active" : ""?>"><a class="page-link" href="index.php?action=all-product&&page=<?=$item?>"><?=$item?></a></li>
+                            <li class="page-item <?=$_GET['page'] == $item ? "active" : ""?>"><a class="page-link" href="index.php?action=all-product&&page=<?=$item?>"><?=$item?></a></li>
                         <?php endfor?>
                     </ul>
                 </nav>
